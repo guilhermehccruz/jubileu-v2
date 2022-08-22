9295fbf98482e346082918d6d4cb60648efa8990
guilhermehccruz <guihccruz60@gmail.com>
2022-08-21 22:28:55 -0300

    fix eslint and remove ts-node-dev for ts-node

---
+++ .eslintrc.cjs
@@ -0,0 +1,102 @@
+module.exports = {
+	'env': {
+		'es2022': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		"plugin:@typescript-eslint/eslint-recommended",
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		"plugin:@typescript-eslint/strict",
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+		'tsconfigRootDir': __dirname,
+		'project': ['./tsconfig.json'],
+	},
+	'plugins': [
+		'@typescript-eslint',
+		'import',
+		'eslint-plugin-import-helpers',
+		'prettier'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'camelcase': 'off',
+		'import/no-unresolved': 'error',
+		'@typescript-eslint/naming-convention': [
+			'error',
+			{
+				'selector': 'interface',
+				'format': ['PascalCase'],
+				'custom': {
+					'regex': '^I[A-Z]',
+					'match': true
+				}
+			}
+		],
+		'class-methods-use-this': 'off',
+		'import/prefer-default-export': 'off',
+		'no-shadow': 'off',
+		'no-console': 'off',
+		'no-useless-constructor': 'off',
+		'no-empty-function': 'off',
+		'lines-between-class-members': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				'ts': 'never'
+			}
+		],
+		'import-helpers/order-imports': [
+			'warn',
+			{
+				'newlinesBetween': 'always',
+				'groups': ['module', '/^@shared/', ['parent', 'sibling', 'index']],
+				'alphabetize': { 'order': 'asc', 'ignoreCase': true }
+			}
+		],
+		'import/no-extraneous-dependencies': [
+			'error',
+			{ 'devDependencies': ['**/*.spec.js'] }
+		],
+		'prettier/prettier': [
+			'error',
+			{
+				'singleQuote': true,
+				'printWidth': 120,
+				'useTabs': true,
+				'tabWidth': 4,
+			}
+		]
+	},
+	'settings': {
+		'import/resolver': {
+			'typescript': {}
+		}
+	},
+	'root': true,
+};
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { dirname, importx } from '@discordx/importer';
+import { IntentsBitField } from 'discord.js';
+import { Client } from 'discordx';
+
+import 'dotenv/config';
+import 'reflect-metadata';
+
+const bot = new Client({
+	// To only use global commands (use @Guild for specific guild command), comment this line
+	botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+
+	// Discord intents
+	intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMembers],
+
+	// Debug logs are disabled in silent mode
+	silent: false,
+
+	// Configuration for @SimpleCommand
+	simpleCommand: {
+		prefix: '.',
+	},
+});
+
+bot.once('ready', async () => {
+	// Make sure all guilds are cached
+	await bot.guilds.fetch();
+
+	// Synchronize applications commands with Discord
+	await bot.initApplicationCommands();
+
+	console.log('>> Bot started');
+});
+
+async function run() {
+	await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
+
+	// Let's start the bot
+	if (!process.env.BOT_TOKEN) {
+		throw Error('Could not find BOT_TOKEN in your environment');
+	}
+
+	// Log in with your bot token
+	await bot.login(process.env.BOT_TOKEN);
+}
+
+await run();
