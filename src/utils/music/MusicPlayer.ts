6cb425c8756a86fe95cc33408841125bae52a3b2
guilhermehccruz <guihccruz60@gmail.com>
2024-09-21 17:48:56 -0300

    refactor: improve MusicPlayer parseCommand returns

--- src/utils/music/MusicPlayer.ts
+++ src/utils/music/MusicPlayer.ts
@@ -1,5 +1,5 @@
 import { QueueManager } from '@discordx/lava-queue';
-import type { ButtonInteraction, CommandInteraction, Guild, TextBasedChannel, TextChannel } from 'discord.js';
+import type { ButtonInteraction, CommandInteraction, Guild, TextChannel } from 'discord.js';
 import { GuildMember } from 'discord.js';
 import { Discord } from 'discordx';
 
@@ -18,39 +18,36 @@ export class MusicPlayer {
 		return this.queueManager.queue(guildId, () => queue);
 	}
 
-	async parseCommand(interaction: CommandInteraction | ButtonInteraction): Promise<
-		| {
-				channel: TextBasedChannel;
-				guild: Guild;
-				member: GuildMember;
-				queue: MusicQueue;
-		  }
-		| undefined
-	> {
+	async parseCommand(interaction: CommandInteraction | ButtonInteraction): Promise<ParsedCommand | null> {
 		await interaction.deferReply();
 
 		if (!interaction.channel || !(interaction.member instanceof GuildMember) || !interaction.guild) {
 			await interaction.followUp('> Não foi possível processar o comando. Tente novamente');
-			return;
+			return null;
 		}
 
 		if (!interaction.member.voice.channelId) {
 			await interaction.followUp('> Entre em um canal de voz primeiro.');
-			return;
+			return null;
 		}
 
 		const bot = interaction.guild.members.cache.get(interaction.client.user.id);
 
 		if (!bot) {
 			await interaction.followUp('> Onde é que eu to? Será que estou em Alagoinha?');
-			return;
+			return null;
 		}
 
 		const queue = this.getQueue(interaction.guild.id);
 
 		if (!queue) {
 			await interaction.followUp('> O reprodutor de música ainda não está pronto. Tente novamente mais tarde');
-			return;
+			return null;
+		}
+
+		if (bot.voice.channelId && interaction.member.voice.channelId !== bot.voice.channelId) {
+			await interaction.followUp('> Entre no mesmo canal de voz que eu primeiro');
+			return null;
 		}
 
 		if (bot.voice.channelId === null) {
@@ -60,16 +57,20 @@ export class MusicPlayer {
 				channel: interaction.member.voice.channelId,
 				deaf: true,
 			});
-		} else if (interaction.member.voice.channelId !== bot.voice.channelId) {
-			await interaction.followUp('> Entre no mesmo canal de voz que eu primeiro');
-			return;
 		}
 
 		return {
-			channel: interaction.channel,
+			channel: interaction.channel as TextChannel,
 			guild: interaction.guild,
 			member: interaction.member,
 			queue,
 		};
 	}
 }
+
+export interface ParsedCommand {
+	channel: TextChannel;
+	guild: Guild;
+	member: GuildMember;
+	queue: MusicQueue;
+  }
