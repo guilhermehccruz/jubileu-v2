9697c1e6e040afff91acee8e28355af8fae0d277
guilhermehccruz <guihccruz60@gmail.com>
2026-01-30 19:03:51 -0300

    feat: add new eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,3 @@
-// @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from "eslint-config-prettier/flat";
@@ -10,8 +8,18 @@ export default tseslint.config(
 	tseslint.configs.stylistic,
 	eslintConfigPrettier,
 	{
+		// Habilita o suporte a tipos (necessário para no-floating-promises)
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
 		ignores: ['dist', 'node_modules'],
 		rules: {
+			'@typescript-eslint/no-floating-promises': ['error', {
+				ignoreVoid: true
+			}],
 			'@typescript-eslint/no-non-null-assertion': 'off',
 			'@typescript-eslint/no-unused-expressions': ['error', {
 				allowShortCircuit: true,
